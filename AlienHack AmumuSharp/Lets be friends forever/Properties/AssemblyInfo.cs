40e2f4b902ec5f9d819858564013f5ee530d68ee
AlienHack <the4got10@windowslive.com>
2014-10-28 17:13:36 +0700

    Version Update

--- AlienHack AmumuSharp/Lets be friends forever/Properties/AssemblyInfo.cs
+++ AlienHack AmumuSharp/Lets be friends forever/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lets be friends forever")]
+[assembly: AssemblyTitle("AlienHack AmumuSharp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lets be friends forever")]
+[assembly: AssemblyProduct("AlienHack AmumuSharp")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
